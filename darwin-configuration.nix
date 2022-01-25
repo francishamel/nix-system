caa01d3470857f580e1498a210a5df7c9ef5ecd1
Francis Hamel <francishamel96@gmail.com>
2022-01-24 20:59:49 -0500

    Setup dock settings (#14)

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -158,6 +158,17 @@
   system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
   system.defaults.NSGlobalDomain."com.apple.trackpad.scaling" = "1.25";
 
+  system.defaults.dock = {
+    autohide = true;
+    autohide-delay = "0.1";
+    autohide-time-modifier = "0.5";
+    enable-spring-load-actions-on-all-items = false;
+    launchanim = false;
+    mru-spaces = false;
+    show-process-indicators = false;
+    static-only = true;
+  };
+
   networking = {
     dns = [
       "1.1.1.1" # Cloudflare's primary dns
