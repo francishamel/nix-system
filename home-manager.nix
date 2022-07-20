d7c992ce2adf4a2c7a492f1ed9cf78377e1a9952
Francis Hamel <francishamel96@gmail.com>
2022-07-20 13:52:21 -0400

    Setup 1password identity agent for ssh (#52)

--- home-manager.nix
+++ home-manager.nix
@@ -87,6 +87,13 @@
       enableAliases = true;
     };
 
+    ssh = {
+      enable = true;
+      extraConfig = ''
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      '';
+    };
+
     starship.enable = true;
 
     tmux.enable = true;
