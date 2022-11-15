cd5901b87628f134c729ec63d36d4b9446d283e6
Francis Hamel <francishamel96@gmail.com>
2022-11-15 02:42:20 -0500

    Fix wrongly set SSH_AUTH_LOCK env var

--- 1password.nix
+++ 1password.nix
@@ -14,12 +14,9 @@
 
     zsh = {
       initExtra = ''
+        SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
         eval "$(op completion zsh)"; compdef _op op
       '';
-
-      sessionVariables = {
-        SSH_AUTH_SOCK = "~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-      };
     };
   };
 }
