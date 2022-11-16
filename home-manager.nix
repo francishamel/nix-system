52e366ab2654f0a976b540c0063bc092710d42a1
Francis Hamel <francishamel96@gmail.com>
2022-11-15 21:43:56 -0500

    Extract vscode config to its own file

--- home-manager.nix
+++ home-manager.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./1password.nix
+    ./vscode.nix
   ];
 
   home = {
@@ -120,43 +121,6 @@
     starship.enable = true;
 
     tmux.enable = true;
-
-    vscode = {
-      enable = true;
-      extensions = with pkgs.vscode-extensions; [
-        alefragnani.project-manager
-        eamodio.gitlens
-        jnoortheen.nix-ide
-        pkief.material-icon-theme
-        redhat.vscode-yaml
-      ];
-      userSettings = {
-        "diffEditor.ignoreTrimWhitespace" = false;
-        "editor.fontFamily" = "'FiraCode Nerd Font'";
-        "editor.fontLigatures" = true;
-        "editor.formatOnSave" = true;
-        "editor.minimap.enabled" = false;
-        "editor.tabSize" = 2;
-        "elixirLS.dialyzerEnabled" = false;
-        "elixirLS.suggestSpecs" = false;
-        "extensions.autoUpdate" = false;
-        "files.insertFinalNewline" = true;
-        "git.allowForcePush" = true;
-        "git.branchProtection" = [ "main" "master" "trunk" ];
-        "git.branchProtectionPrompt" = "alwaysCommitToNewBranch";
-        "git.confirmSync" = false;
-        "git.experimental.mergeEditor" = true;
-        "git.terminalAuthentication" = false;
-        "git.untrackedChanges" = "separate";
-        "github.gitAuthentication" = false;
-        "githubPullRequests.pushBranch" = "always";
-        "projectManager.git.baseFolders" = [ "~/.nixpkgs" "~/src" ];
-        "telemetry.telemetryLevel" = "off";
-        "update.mode" = "none";
-        "workbench.colorTheme" = "Solarized Dark";
-        "workbench.iconTheme" = "material-icon-theme";
-      };
-    };
 
     zsh = {
       dotDir = ".config/zsh";
---
+++ vscode.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      alefragnani.project-manager
+      eamodio.gitlens
+      jnoortheen.nix-ide
+      pkief.material-icon-theme
+      redhat.vscode-yaml
+    ];
+    userSettings = {
+      "diffEditor.ignoreTrimWhitespace" = false;
+      "editor.fontFamily" = "'FiraCode Nerd Font'";
+      "editor.fontLigatures" = true;
+      "editor.formatOnSave" = true;
+      "editor.minimap.enabled" = false;
+      "editor.tabSize" = 2;
+      "elixirLS.dialyzerEnabled" = false;
+      "elixirLS.suggestSpecs" = false;
+      "extensions.autoUpdate" = false;
+      "files.insertFinalNewline" = true;
+      "git.allowForcePush" = true;
+      "git.branchProtection" = [ "main" "master" "trunk" ];
+      "git.branchProtectionPrompt" = "alwaysCommitToNewBranch";
+      "git.confirmSync" = false;
+      "git.experimental.mergeEditor" = true;
+      "git.terminalAuthentication" = false;
+      "git.untrackedChanges" = "separate";
+      "github.gitAuthentication" = false;
+      "githubPullRequests.pushBranch" = "always";
+      "projectManager.git.baseFolders" = [ "~/.nixpkgs" "~/src" ];
+      "telemetry.telemetryLevel" = "off";
+      "update.mode" = "none";
+      "workbench.colorTheme" = "Solarized Dark";
+      "workbench.iconTheme" = "material-icon-theme";
+    };
+  };
+}
