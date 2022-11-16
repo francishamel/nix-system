66d0cce8b461494d56c8efcd99567b4b8e960a09
Francis Hamel <francishamel96@gmail.com>
2022-11-15 23:38:56 -0500

    Move home-manager config to its own dir

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -2,14 +2,7 @@
 
 {
   imports = [
-    ./1password.nix
-    ./alacritty.nix
-    ./bat.nix
-    ./direnv.nix
-    ./git.nix
-    ./lsd.nix
-    ./vscode.nix
-    ./zsh.nix
+    ./programs
   ];
 
   home = {
---
+++ home-manager/programs/default.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  imports = [
+    ./1password.nix
+    ./alacritty.nix
+    ./bat.nix
+    ./direnv.nix
+    ./git.nix
+    ./lsd.nix
+    ./vscode.nix
+    ./zsh.nix
+  ];
+}
