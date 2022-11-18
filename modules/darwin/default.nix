54d9a1c14f82e3728119190f7bfa3dd84ac409cc
Francis Hamel <francishamel96@gmail.com>
2022-11-18 00:14:29 -0500

    Move homebrew config to its own file

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./homebrew.nix
+  ];
+
   users = {
     knownUsers = [
       "francis"
@@ -23,27 +27,6 @@
           ];
         }
       )
-    ];
-  };
-
-  homebrew = {
-    onActivation = {
-      autoUpdate = false;
-      cleanup = "zap";
-    };
-    enable = true;
-    casks = [
-      "1password"
-      # Disabled 1password-cli to manually install the beta package which is not on homebrew
-      # "1password-cli"
-      "discord"
-      "firefox"
-      "obsidian"
-      "raycast"
-      "slack"
-      "spotify"
-      "todoist"
-      "zoom"
     ];
   };
 
---
+++ modules/darwin/homebrew.nix
@@ -0,0 +1,23 @@
+{ ... }:
+{
+  homebrew = {
+    onActivation = {
+      autoUpdate = false;
+      cleanup = "zap";
+    };
+    enable = true;
+    casks = [
+      "1password"
+      # Disabled 1password-cli to manually install the beta package which is not on homebrew
+      # "1password-cli"
+      "discord"
+      "firefox"
+      "obsidian"
+      "raycast"
+      "slack"
+      "spotify"
+      "todoist"
+      "zoom"
+    ];
+  };
+}
