7f8e5420281494f0acd57e827d948ec81c38819d
Francis Hamel <francishamel96@gmail.com>
2022-11-20 01:17:31 -0500

    Enable fzf

---
+++ modules/home-manager/programs/fzf.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [ zsh-fzf-tab ];
+
+  programs.zsh.initExtra = ''
+    source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+  '';
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+}
