54f15df54abd1685f19e4b5430f1e9856b5a28d0
Francis Hamel <francishamel96@gmail.com>
2022-12-22 17:04:19 -0500

    Remove 1password-cli beta

--- configs/darwin/macbook-pro.nix
+++ configs/darwin/macbook-pro.nix
@@ -52,8 +52,7 @@
     enable = true;
     casks = [
       "1password"
-      # Disabled 1password-cli to manually install the beta package which is not on homebrew
-      # "1password-cli"
+      "1password-cli"
       "discord"
       "firefox"
       "obsidian"
