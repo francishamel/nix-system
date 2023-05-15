87fe637ae8b3a7847a4b654d8429866bdfa66ec4
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2023-05-15 11:30:28 -0400

    Add gh alias to clone repos in default folder

--- modules/home-manager/cli/gh/default.nix
+++ modules/home-manager/cli/gh/default.nix
@@ -15,6 +15,7 @@ in
         aliases = {
           "prc" = "pr create --web --assignee @me";
           "prv" = "pr view --web";
+          "rc" = "repo clone $1 ${config.home.homeDirectory}/src/$1";
         };
         editor = "code --wait";
         git_protocol = "ssh";
