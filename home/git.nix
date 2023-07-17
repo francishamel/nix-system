235ececafb37b3e1e06ebf074f0578a577cd593c
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2023-07-17 10:51:07 -0400

    Add git alias

--- home/git.nix
+++ home/git.nix
@@ -4,7 +4,10 @@
   programs.git = {
     userName = "Francis Hamel";
     userEmail = "36383308+francishamel@users.noreply.github.com";
-    aliases = { fp = "fetch --prune"; };
+    aliases = {
+      fp = "fetch --prune";
+      pfwl = "push --force-with-lease";
+    };
     enable = true;
     extraConfig = {
       core.editor = "code --wait";
