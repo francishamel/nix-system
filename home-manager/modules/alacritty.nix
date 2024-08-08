67cec99dda2a7a347a0983d1237eb8c9993e60a0
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-08-08 12:43:17 -0400

    Fix alacritty keybindings

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -10,6 +10,16 @@
         startup_mode = "Maximized";
         title = "Alacritty";
       };
+
+      # See https://portal.cs.umbc.edu/help/theory/ascii.txt
+      keyboard.bindings = [
+        { key = "Backspace"; mods = "Command"; chars = "\\u0015"; }
+        { key = "Backspace"; mods = "Alt"; chars = "\\u0017"; }
+        { key = "Left"; mods = "Command"; chars = "\\u0001"; }
+        { key = "Right"; mods = "Command"; chars = "\\u0005"; }
+        { key = "Left"; mods = "Alt"; chars = "\\u001b\\u0062"; }
+        { key = "Right"; mods = "Alt"; chars = "\\u001b\\u0066"; }
+      ];
     };
   };
 }
