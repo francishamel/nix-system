f615af2def4f5f51196c8ebb9363f94e44cc7829
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-09-06 17:21:49 -0400

    Setup yazi-picker for helix
    Used https://yazi-rs.github.io/docs/tips/#helix-with-zellij to set this up.

--- home-manager/modules/helix.nix
+++ home-manager/modules/helix.nix
@@ -1,5 +1,20 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  yaziPicker = pkgs.writeShellScriptBin "yazi-picker" ''
+    paths=$(${pkgs.yazi}/bin/yazi --chooser-file=/dev/stdout | while read -r; do printf "%q " "$REPLY"; done)
+
+    if [[ -n "$paths" ]]; then
+    	${pkgs.zellij}/bin/zellij action toggle-floating-panes
+    	${pkgs.zellij}/bin/zellij action write 27 # send <Escape> key
+    	${pkgs.zellij}/bin/zellij action write-chars ":open $paths"
+    	${pkgs.zellij}/bin/zellij action write 13 # send <Enter> key
+    	${pkgs.zellij}/bin/zellij action toggle-floating-panes
+    fi
+
+    ${pkgs.zellij}/bin/zellij action close-pane
+  '';
+in
 {
   programs.helix = {
     enable = true;
@@ -14,6 +29,9 @@
     };
     settings = {
       editor.line-number = "relative";
+      keys.normal = {
+        "C-y" = lib.mkIf config.programs.yazi.enable ":sh ${pkgs.zellij}/bin/zellij run -f -n yazi-picker -x 10% -y 10% --width 80% --height 80% -- ${yaziPicker}/bin/yazi-picker";
+      };
     };
   };
 }
