0a67feaeee6558a3e7b55e9861d30dcdda6cba15
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-09-07 18:41:39 -0400

    Set default zellij layout to compact

--- home-manager/modules/zellij.nix
+++ home-manager/modules/zellij.nix
@@ -5,6 +5,7 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
+      default_layout = "compact";
       on_force_close = "quit";
       ui.pane_frames.rounded_corners = true;
     };
