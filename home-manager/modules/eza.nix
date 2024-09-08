bf57e60096de6347eeb141f0caf4ffeeeac2caaf
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-09-07 22:39:14 -0400

    Replace lsd with eza (#308)

---
+++ home-manager/modules/eza.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  programs.eza = {
+    enable = true;
+    enableZshIntegration = true;
+    extraOptions = [
+      "--group"
+      "--group-directories-first"
+      "--header"
+      "--hyperlink"
+    ];
+    git = true;
+    icons = true;
+  };
+}
