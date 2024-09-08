7d98ac19aa466edc8343be21a25ff3a4a61db5d4
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-09-08 00:00:18 -0400

    Set wezterm theme

--- home-manager/modules/wezterm/wezterm.lua
+++ home-manager/modules/wezterm/wezterm.lua
@@ -1,12 +1,16 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 
 wezterm.on("gui-startup", function()
 	local tab, pane, window = wezterm.mux.spawn_window({})
 	window:gui_window():maximize()
 end)
 
+local config = wezterm.config_builder()
+
+config.color_scheme = "nord"
 config.enable_tab_bar = false
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 14.0
 config.front_end = "WebGpu"
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
