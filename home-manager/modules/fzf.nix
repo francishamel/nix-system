67df50fd48a02661208be36c614aec0a6877a566
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-09-07 23:52:16 -0400

    Set fzf theme

--- home-manager/modules/fzf.nix
+++ home-manager/modules/fzf.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }:
 
 {
-  home.packages = [
-    pkgs.zsh-fzf-tab
-  ];
+  home = {
+    packages = [ pkgs.zsh-fzf-tab ];
+    sessionVariables.FZF_DEFAULT_OPTS = "--color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1";
+  };
 
-  programs.zsh.initExtra = ''
-    source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-  '';
-
-  programs.fzf.enable = true;
+  programs = {
+    fzf.enable = true;
+    zsh.initExtra = ''
+      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+    '';
+  };
 }
