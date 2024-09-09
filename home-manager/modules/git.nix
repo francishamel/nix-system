1ca535bb8966e01f8b2d555e0f17ee71b773c0e7
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-09-08 23:31:58 -0400

    Setup lazygit (#316)
    [git] Setup git TUI
    
    I'm now using lazygit.

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,43 +1,47 @@
 { config, ... }:
 
 {
-  programs.git = {
-    userName = "Francis Hamel";
-    userEmail = "36383308+francishamel@users.noreply.github.com";
-    aliases = {
-      ca = "commit --amend --no-edit";
-      cae = "commit --amend";
-      cf = "commit --fixup";
-      fp = "fetch --prune";
-      pfwl = "push --force-with-lease";
-      ri = "rebase --interactive";
-    };
-    enable = true;
-    extraConfig = {
-      init.defaultBranch = "main";
-      merge.conflictstyle = "diff3";
-      pull.rebase = true;
-      rebase = {
-        autoStash = true;
-        autosquash = true;
-      };
-      rerere.enabled = true;
-    };
-  };
-
-  programs.gh = {
-    enable = true;
-    gitCredentialHelper.enable = false;
-    settings = {
+  programs = {
+    git = {
+      userName = "Francis Hamel";
+      userEmail = "36383308+francishamel@users.noreply.github.com";
       aliases = {
-        "ic" = "issue create --web";
-        "id" = "issue develop $1 --checkout";
-        "il" = "issue list";
-        "prc" = "pr create --web --assignee @me";
-        "prv" = "pr view --web";
-        "rc" = "repo clone $1 ${config.home.homeDirectory}/src/$1";
+        ca = "commit --amend --no-edit";
+        cae = "commit --amend";
+        cf = "commit --fixup";
+        fp = "fetch --prune";
+        pfwl = "push --force-with-lease";
+        ri = "rebase --interactive";
       };
-      git_protocol = "ssh";
+      enable = true;
+      extraConfig = {
+        init.defaultBranch = "main";
+        merge.conflictstyle = "diff3";
+        pull.rebase = true;
+        rebase = {
+          autoStash = true;
+          autosquash = true;
+        };
+        rerere.enabled = true;
+      };
     };
+
+    gh = {
+      enable = true;
+      gitCredentialHelper.enable = false;
+      settings = {
+        aliases = {
+          "ic" = "issue create --web";
+          "id" = "issue develop $1 --checkout";
+          "il" = "issue list";
+          "prc" = "pr create --web --assignee @me";
+          "prv" = "pr view --web";
+          "rc" = "repo clone $1 ${config.home.homeDirectory}/src/$1";
+        };
+        git_protocol = "ssh";
+      };
+    };
+
+    lazygit.enable = true;
   };
 }
