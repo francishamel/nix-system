ac008218b22390c5cba3ead794cf92eaccd0e2f1
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-10-02 14:21:33 -0400

    Map keys to what I am used to
    - opt-left for back one word
    - opt-right for forward one word
    - cmd-left for beginning of line
    - cmd-right for end of line
    - opt-backspace for deleting previous word
    - cmd-backspace for deleting whole line

--- home-manager/modules/wezterm/wezterm.lua
+++ home-manager/modules/wezterm/wezterm.lua
@@ -16,4 +16,57 @@ config.front_end = "WebGpu"
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 
+local act = wezterm.action
+
+config.keys = {
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "b",
+			mods = "ALT",
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "f",
+			mods = "ALT",
+		}),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = act.SendKey({
+			key = "a",
+			mods = "CTRL",
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = act.SendKey({
+			key = "e",
+			mods = "CTRL",
+		}),
+	},
+	{
+		key = "Backspace",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "w",
+			mods = "CTRL",
+		}),
+	},
+	{
+		key = "Backspace",
+		mods = "CMD",
+		action = act.SendKey({
+			key = "u",
+			mods = "CTRL",
+		}),
+	},
+}
+
 return config
