aef5c1b584ef6239ee755534bb7d1c2e5a770c7f
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-12-02 13:02:56 -0500

    Disable movement keys in normal mode (except for HJKL)

--- home-manager/modules/helix.nix
+++ home-manager/modules/helix.nix
@@ -51,6 +51,14 @@ in
       };
       keys = {
         normal = {
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+          pageup = "no_op";
+          pagedown = "no_op";
+          home = "no_op";
+          end = "no_op";
           "C-y" = lib.mkIf (config.programs.yazi.enable && config.programs.zellij.enable) ":sh ${pkgs.zellij}/bin/zellij run -f -n yazi-picker -x 10% -y 10% --width 80% --height 80% -- ${yaziPicker}/bin/yazi-picker";
         };
         insert = {
