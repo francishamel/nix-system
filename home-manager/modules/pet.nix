91e0139c4c979b10f53a8a4799a81abaa0271c54
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-12-02 13:03:05 -0500

    Add shell alias for pet exec

--- home-manager/modules/pet.nix
+++ home-manager/modules/pet.nix
@@ -1,20 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.pet = {
-    enable = true;
+  programs = {
+    pet = {
+      enable = true;
 
-    snippets = [
-      {
-        command = "bin/rails g migration <name>";
-        description = "Generate a Rails migration";
-        tag = [ "rails" ];
-      }
-      {
-        command = "bin/rails g maintenance_tasks:task <name>";
-        description = "Generate a Rails maintenance task";
-        tag = [ "rails" ];
-      }
-    ];
+      snippets = [
+        {
+          command = "bin/rails g migration <name>";
+          description = "Generate a Rails migration";
+          tag = [ "rails" ];
+        }
+        {
+          command = "bin/rails g maintenance_tasks:task <name>";
+          description = "Generate a Rails maintenance task";
+          tag = [ "rails" ];
+        }
+      ];
+    };
+
+    zsh.shellAliases."pe" = "${pkgs.pet}/bin/pet exec";
   };
 }
