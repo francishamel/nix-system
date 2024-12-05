3da8cf1b26aae199167ed3135e372ef13389a542
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-12-05 16:55:21 -0500

    Add more pet commands (#335)

--- home-manager/modules/pet.nix
+++ home-manager/modules/pet.nix
@@ -9,12 +9,27 @@
         {
           command = "bin/rails g migration <name>";
           description = "Generate a Rails migration";
-          tag = [ "rails" ];
+          tag = [ "rails" "db" ];
         }
         {
           command = "bin/rails g maintenance_tasks:task <name>";
           description = "Generate a Rails maintenance task";
           tag = [ "rails" ];
+        }
+        {
+          command = "bin/rails db:migrate:status";
+          description = "Display status of Rails migrations";
+          tag = [ "rails" "db" ];
+        }
+        {
+          command = "bin/rails db:migrate:down VERSION=<migration version>";
+          description = "Rollback a specific Rails db migration";
+          tag = [ "rails" "db" ];
+        }
+        {
+          command = "bin/rails db:rollback STEP=<step=1>";
+          description = "Rollback STEP Rails db migration(s)";
+          tag = [ "rails" "db" ];
         }
       ];
     };
