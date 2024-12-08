31f63aef3be8747084ed3c2061f76bd3b86f6945
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-12-08 18:15:10 -0500

    Replace .. alias from cd to z

--- home-manager/modules/zsh/common.nix
+++ home-manager/modules/zsh/common.nix
@@ -5,7 +5,6 @@
     dotDir = ".config/zsh";
     enable = true;
     historySubstringSearch.enable = true;
-    shellAliases.".." = "cd ..";
     initExtra = ''
       bindkey '^U' backward-kill-line
     '';
