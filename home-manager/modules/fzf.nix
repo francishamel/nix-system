e1e4f0ca46a8df0c77cc9ad5d9574c078f4c9ab0
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2024-12-20 02:01:36 -0500

    Use proper option for setting color scheme

--- home-manager/modules/fzf.nix
+++ home-manager/modules/fzf.nix
@@ -1,13 +1,26 @@
 { pkgs, ... }:
 
 {
-  home = {
-    packages = [ pkgs.zsh-fzf-tab ];
-    sessionVariables.FZF_DEFAULT_OPTS = "--color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1";
-  };
+  home.packages = [ pkgs.zsh-fzf-tab ];
 
   programs = {
-    fzf.enable = true;
+    fzf = {
+      enable = true;
+      colors = {
+        "bg+" = "#3B4252";
+        "bg" = "#2E3440";
+        "spinner" = "#81A1C1";
+        "hl" = "#616E88";
+        "fg" = "#D8DEE9";
+        "header" = "#616E88";
+        "info" = "#81A1C1";
+        "pointer" = "#81A1C1";
+        "marker" = "#81A1C1";
+        "fg+" = "#D8DEE9";
+        "prompt" = "#81A1C1";
+        "hl+" = "#81A1C1";
+      };
+    };
     zsh.initExtra = ''
       source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
     '';
