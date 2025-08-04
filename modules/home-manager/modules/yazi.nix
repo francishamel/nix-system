5d0ad0330aaa34bed10cae1d399a7ec5a30b9fa9
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2025-08-04 15:01:21 -0400

    Add zathura (#366)
    * Add Zathura PDF viewer
    
    * Configure yazi to open pdf with zathura

--- modules/home-manager/modules/yazi.nix
+++ modules/home-manager/modules/yazi.nix
@@ -1,8 +1,27 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      open = {
+        prepend_rules = [
+          {
+            mime = "application/pdf";
+            use = [ "pdff" ];
+          }
+        ];
+      };
+      opener = {
+        pdff = [
+          {
+            run = "${pkgs.zathura}/bin/zathura --mode fullscreen \"$@\"";
+            desc = "zathura";
+            block = true;
+          }
+        ];
+      };
+    };
   };
 }
