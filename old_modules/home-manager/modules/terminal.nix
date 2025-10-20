b610c9d396d5af09c0dce44601fc2a458304a1a9
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2025-10-19 20:42:02 -0400

    Remove unused packages

--- old_modules/home-manager/modules/terminal.nix
+++ old_modules/home-manager/modules/terminal.nix
@@ -3,14 +3,10 @@
 {
   home.packages = [
     pkgs.cachix
-    pkgs.d2
     pkgs.devenv
     pkgs.flyctl
-    pkgs.glances
     pkgs.glow
     pkgs.google-cloud-sdk
     pkgs.just
-    pkgs.tokei
-    pkgs.xh
   ];
 }
