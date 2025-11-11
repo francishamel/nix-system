ce9daa56e963bfdee1212a2973376b3cb7e7dd83
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2025-11-10 20:23:38 -0500

    Remove wezterm

--- modules/terminal/wezterm.nix
+++
@@ -1,9 +0,0 @@
-{
-  flake.modules.homeManager.gui = {
-    programs.wezterm = {
-      enable = false;
-      enableZshIntegration = true;
-      extraConfig = builtins.readFile ./wezterm/config.lua;
-    };
-  };
-}
