df4d39011be005c9a882c8763ac9f99691a189c3
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2025-11-14 10:02:59 -0500

    Temp fix for helix typescript LSP deadlock

--- modules/text-editors/helix.nix
+++ modules/text-editors/helix.nix
@@ -14,6 +14,22 @@
         end = "no_op";
       };
       prettier = lib.getExe pkgs.prettier;
+      # Temporary fix for LSP issue
+      # https://github.com/helix-editor/helix/issues/14738
+      # https://github.com/typescript-language-server/typescript-language-server/issues/1014
+      typescript-language-server = pkgs.typescript-language-server.overrideAttrs (oldAttrs: rec {
+        version = "5.0.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "typescript-language-server";
+          repo = "typescript-language-server";
+          rev = "v5.0.1";
+          hash = "sha256-Ziiiw6MXoIa1bWtME7dvzg+kQ8iXMG3P5rNR1B/Iifg=";
+        };
+        offlineCache = pkgs.fetchYarnDeps {
+          yarnLock = "${src}/yarn.lock";
+          hash = "sha256-ODO1G1AJd38cGqHhau1t4D8Mrug44pLk36d9dGtb/nM=";
+        };
+      });
     in
     {
       programs.helix = {
@@ -33,7 +49,7 @@
           pkgs.python312Packages.python-lsp-server
           pkgs.taplo
           pkgs.tinymist
-          pkgs.typescript-language-server
+          typescript-language-server
           pkgs.yaml-language-server
         ];
         # TODO: languages concern
