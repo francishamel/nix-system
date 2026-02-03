e4714407b0f580b90ba84ac174ad18a056718fc1
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2026-02-03 13:59:29 -0500

    Use nixpkgs typescript-language-server version

--- modules/languages/javascript/helix.nix
+++ modules/languages/javascript/helix.nix
@@ -10,29 +10,12 @@
           "typescript"
         ];
       };
-      # Temporary fix for LSP issue
-      # https://github.com/helix-editor/helix/issues/14738
-      # https://github.com/typescript-language-server/typescript-language-server/issues/1014
-      # There's a fix in 5.1.2, just waiting on nixpkgs to catch up
-      typescript-language-server = pkgs.typescript-language-server.overrideAttrs (_: rec {
-        version = "5.1.2";
-        src = pkgs.fetchFromGitHub {
-          owner = "typescript-language-server";
-          repo = "typescript-language-server";
-          rev = "v${version}";
-          hash = "sha256-8UDPeW8Bb6Or+G28GI+fprUtqnDGKTqeWskpn9i0HCA=";
-        };
-        offlineCache = pkgs.fetchYarnDeps {
-          yarnLock = "${src}/yarn.lock";
-          hash = "sha256-CSjxiuUN+hHmoWwkVe6c5lLFeX3ROB3QlBQ15rVmPhk=";
-        };
-      });
     in
     {
       programs.helix = {
         extraPackages = [
           pkgs.prettier
-          typescript-language-server
+          pkgs.typescript-language-server
         ];
         languages.language = [
           {
