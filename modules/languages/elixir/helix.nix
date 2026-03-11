bd67b985229ecc88ca4398adb24792f885a1dee3
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2026-03-11 14:13:29 -0400

    Remove mix format for elixir

--- modules/languages/elixir/helix.nix
+++ modules/languages/elixir/helix.nix
@@ -1,20 +1,7 @@
-{ lib, ... }:
 {
   flake.modules.homeManager.base =
     { pkgs, ... }:
     {
-      programs.helix = {
-        extraPackages = [ pkgs.elixir-ls ];
-        languages.language = [
-          {
-            name = "elixir";
-            auto-format = true;
-            formatter = {
-              command = lib.getExe' pkgs.elixir "mix";
-              args = [ "format" ];
-            };
-          }
-        ];
-      };
+      programs.helix.extraPackages = [ pkgs.elixir-ls ];
     };
 }
