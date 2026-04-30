cb712c266821c1ee2abc62dd291e5d1a0278d526
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2026-04-30 18:32:58 -0400

    Install a cheat repo for navi

--- modules/navi.nix
+++ modules/navi.nix
@@ -1,6 +1,18 @@
 {
-  flake.modules.homeManager.base.programs.navi = {
-    enable = true;
-    enableZshIntegration = true;
-  };
+  flake.modules.homeManager.base =
+    { pkgs, ... }:
+    {
+      programs.navi = {
+        enable = true;
+        enableZshIntegration = true;
+        settings.cheats.paths = [
+          "${pkgs.fetchFromGitHub {
+            owner = "denisidoro";
+            repo = "cheats";
+            rev = "1339965e9615ce00174cc308a41279d9c59aa75f";
+            hash = "sha256-wPsAazAGKPhu0MZfZbZ0POUBEMg95frClAQERTDFXUg=";
+          }}"
+        ];
+      };
+    };
 }
