fa87fee7f008ac81172828b3f0b5bc50a9e0f490
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2026-05-14 14:49:02 -0400

    Remove unnecessary extra-substituters for devenv
    devenv already implements that

--- modules/devenv.nix
+++ modules/devenv.nix
@@ -1,10 +1,5 @@
 { lib, ... }:
 {
-  nix.settings = {
-    extra-substituters = [ "https://devenv.cachix.org" ];
-    extra-trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
-  };
-
   flake.modules.homeManager.base =
     { pkgs, ... }:
     {
