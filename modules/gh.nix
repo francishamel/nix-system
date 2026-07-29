8f9842d934a508e281cf9d62da4bc3d384dc8310
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2026-07-29 13:14:42 -0400

    Set default branch to main in gh prc alias

--- modules/gh.nix
+++ modules/gh.nix
@@ -11,7 +11,7 @@ in
         gitCredentialHelper.enable = false;
         settings = {
           aliases = {
-            "prc" = "pr create --web --assignee @me --base $1";
+            "prc" = "!gh pr create --web --assignee @me --base \"\${1:-main}\"";
             "prv" = "pr view --web";
             "rc" = "repo clone $1 ${config.home.homeDirectory}/src/gh/$1";
             "rcme" = "repo clone $1 ${config.home.homeDirectory}/src/gh/${user.githubUsername}/$1";
