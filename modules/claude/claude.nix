c157c63411ceb8a710ab844fcd2dc0fb3a12bf22
Francis Hamel <36383308+francishamel@users.noreply.github.com>
2026-09-05 00:08:55 -0400

    Extract numtide cache

--- modules/claude/claude.nix
+++ modules/claude/claude.nix
@@ -2,11 +2,6 @@
 {
   nixpkgs.allowedUnfreePackages = [ "claude-code" ];
 
-  nix.settings = {
-    extra-substituters = [ "https://cache.numtide.com" ];
-    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
-  };
-
   flake = {
     modules = {
       homeManager.base =
